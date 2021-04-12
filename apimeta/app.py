74b46be6e87247a5d2239f290a893b5b2a9e46d3
kiriharu <kiriharu@yandex.ru>
2021-04-12 10:55:25 +0300

    indexpage WIP

--- apimeta/app.py
+++ apimeta/app.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from .schemas import MetaResponseScheme
 from .scraper import SiteScrapper
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="apimeta/static"), name="static")
+templates = Jinja2Templates(directory="apimeta/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", dict(request=request))
 
 
 @app.get("/info")
